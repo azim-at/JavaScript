422be8c68ed9fd716f692d973e55c46d79e42326
azim-at <azeem.talikoti@gmail.com>
2022-05-07 22:52:57 +0530

    types of printing options in js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-hehe
+ alert("hello world")
+console.log("kya re maa");
+console.warn("This is a warning");
+console.error("This is a error");
+
+var n1 = 23
+var n2 = 55
+alert(n1 + n2)
+console.log(n1 + n2)
