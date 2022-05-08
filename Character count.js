5f5b131cedf1f7409fd68de050aa17799a51d7a1
azim-at <azeem.talikoti@gmail.com>
2022-05-08 12:05:13 +0530

    basics

---
+++ Character count.js
@@ -0,0 +1,5 @@
+var message = prompt("Enter your message");
+var storeLength = message.length;
+var wordsLength = 180;
+var remaining= wordsLength - storeLength;
+alert("You have writteen "+storeLength+" characters, you have "+remaining+" characters, left");
---
+++ practice.js
@@ -0,0 +1,4 @@
+function name() {
+    var myName = prompt("Enter your name:");
+    document.write(myName);
+}
