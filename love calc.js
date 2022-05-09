ce6995edb4a071cb5eadf76ff413ab11042bae05
azim-at <azeem.talikoti@gmail.com>
2022-05-09 17:57:08 +0530

    love percentage calculator

--- love calc.js
+++ love calc.js
@@ -2,5 +2,4 @@ firstName = prompt("Enter the First name");
 secondName = prompt("Enter the Second name");
 cal = Math.random();
 cal = Math.floor(cal * 100);
-alert(cal+"%");
-console.log(loveCal);
+alert(firstName+" & "+secondName+" your love percentage is" +cal+"%");
