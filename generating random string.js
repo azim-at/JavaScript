be6be4ea055cefb67ab8c00ec96248cc0ed1e485
azim-at <azeem.talikoti@gmail.com>
2022-05-10 23:41:02 +0530

    generating random array using random()

---
+++ generating random string.js
@@ -0,0 +1,5 @@
+function whosPaying() {
+    var inputArr = ['Angela','Ben','Jenny','Michael','Chloe'];
+    var ran = Math.floor(Math.random() * inputArr.length)
+    return inputArr[ran]+" is going to buy lunch today!"
+    }
