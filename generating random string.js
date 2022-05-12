70eaf6e4d5bdf4a61d868cc85ff5b4f01f6abd6e
azim-at <azeem.talikoti@gmail.com>
2022-05-12 21:46:16 +0530

    random name gen

--- generating random string.js
+++ generating random string.js
@@ -1,5 +1,7 @@
 function whosPaying() {
+
     var inputArr = ['Angela','Ben','Jenny','Michael','Chloe'];
     var ran = Math.floor(Math.random() * inputArr.length)
     return inputArr[ran]+" is going to buy lunch today!"
-    }
+    
+}
