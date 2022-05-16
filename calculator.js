e6c164a70d56635ba4c9daa15fdf974cab3459ff
azim-at <azeem.talikoti@gmail.com>
2022-05-16 21:34:58 +0530

    calling different functions

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(n1,n2) {
+    return n1 + n2;
+}
+
+function mul(n1,n2) {
+    return n1 * n2;
+}
+
+function sub(n1, n2) {
+    return n1 - n2;
+}
+
+function div(n1, n2) {
+    return n1 / n2;
+}
+
+function calculator(n1, n2, operator) {
+    return operator(n1,n2);
+}
