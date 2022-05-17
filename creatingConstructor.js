bb70bff94d21ce143d3145f56e4bd982eed1a5ff
azim-at <azeem.talikoti@gmail.com>
2022-05-17 21:04:22 +0530

    JavaScript Constructor

---
+++ creatingConstructor.js
@@ -0,0 +1,8 @@
+function HouseKeeper(name, address, yearsOfExperience, phoneno) {
+    this.name = name;
+    this.address = address;
+    this.yearsOfExperience = yearsOfExperience;
+    this.phoneno = phoneno;
+}
+
+var houseKeeper1 = new HouseKeeper("Kira","Home",12,9535983796);
